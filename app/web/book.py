a733a36b3e68bfcfd0f949416dcadc9ff5829c58
netcollo <netcollo@gmail.com>
2019-06-01 16:24:28 +0800

    change some

---
+++ app/web/book.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+from yushu_book import YuShuBook
+from helper import is_isbn_or_key
+
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+    return jsonify(result)
--- fisher.py
+++ fisher.py
@@ -1,15 +1,3 @@
 from flask import Flask
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
-from flask import jsonify
 
 app = Flask(__name__)
-
-@app.route('/book/search/<q>/<page>')
-def search(q, page):
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
