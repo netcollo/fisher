4c6dbcff160702e30f003408e40d25afa1252e7c
netcollo <netcollo@gmail.com>
2019-06-01 23:10:28 +0800

    add Blueprint and some

--- app/web/book.py
+++ app/web/book.py
@@ -1,13 +1,14 @@
-from flask import jsonify
-from flask import Blueprint
+from flask import jsonify, request
 
 from yushu_book import YuShuBook
 from helper import is_isbn_or_key
+from . import web
 
-web = Blueprint('web', __name__)
+@web.route('/book/search')
+def search():
+    q = request.args['q']
+    page = request.args['page']
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)
