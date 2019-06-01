37eddef229c3508ddede3e9cf179cc6b35415f02
netcollo <netcollo@gmail.com>
2019-06-01 17:12:53 +0800

    replace isdn to isbn

--- app/web/book.py
+++ app/web/book.py
@@ -1,8 +1,12 @@
 from flask import jsonify
+from flask import Blueprint
+
 from yushu_book import YuShuBook
 from helper import is_isbn_or_key
 
-@app.route('/book/search/<q>/<page>')
+web = Blueprint('web', __name__)
+
+@web.route('/book/search/<q>/<page>')
 def search(q, page):
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
--- helper.py
+++ helper.py
@@ -4,12 +4,12 @@ def is_isbn_or_key(word):
     
     """
     isbn_or_key = 'key'
-    # ISDN ： 一种为13位0至9的数字
+    # isbn ： 一种为13位0至9的数字
     if len(word) == 13 and word.isdigit():
-        isbn_or_key = 'isdn'
+        isbn_or_key = 'isbn'
 
-    # ISDN ： 10位0至9的数字，其中包含不定数量的'-'， 
+    # isbn ： 10位0至9的数字，其中包含不定数量的'-'， 
     short_word = word.replace('-', '')
     if '-' in word and len(short_word) == 10 and short_word.isdigit():
-        isbn_or_key = 'isdn'
+        isbn_or_key = 'isbn'
     return isbn_or_key
