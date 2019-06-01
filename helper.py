53d9dd5af2fe29769b1bbd9a5ded4470dd297aa9
netcollo <netcollo@gmail.com>
2019-06-01 16:08:03 +0800

    add yushu_book.py

--- helper.py
+++ helper.py
@@ -1,15 +1,15 @@
-#判断输入的内容是否为ISDN
-def is_isdn_or_key(word):
+#判断输入的内容是否为isbn
+def is_isbn_or_key(word):
     """
     
     """
-    isdn_or_key = 'key'
+    isbn_or_key = 'key'
     # ISDN ： 一种为13位0至9的数字
     if len(word) == 13 and word.isdigit():
-        isdn_or_key = 'isdn'
+        isbn_or_key = 'isdn'
 
     # ISDN ： 10位0至9的数字，其中包含不定数量的'-'， 
     short_word = word.replace('-', '')
     if '-' in word and len(short_word) == 10 and short_word.isdigit():
-        isdn_or_key = 'isdn'
-    return isdn_or_key
+        isbn_or_key = 'isdn'
+    return isbn_or_key
--- httper.py
+++ httper.py
@@ -1,7 +1,8 @@
 import requests
 
 class HTTP:
-    def get(self, url, return_json = True):
+    @staticmethod
+    def get(url, return_json = True):
         r = requests.get(url)
         if r.status_code != 200:
             return {} if return_json else ''
