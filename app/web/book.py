5c4b03f07aed73af79f0e0a18e70dc78db8ba762
netcollo <netcollo@gmail.com>
2019-06-10 19:02:06 +0800

    add forms

--- app/web/book.py
+++ app/web/book.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request
+from app.forms.book import SearchForm
 
 from yushu_book import YuShuBook
 from helper import is_isbn_or_key
@@ -6,12 +7,21 @@ from . import web
 
 @web.route('/book/search')
 def search():
-    q = request.args['q']
-    page = request.args['page']
+    # # 至少药有一个字符，长度限制
+    # q = request.args['q']
+    # # 正整数，长度限制
+    # page = request.args['page']
 
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+        return jsonify(result)
     else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
+        return jsonify({'msg': '参数校验失败'})
