3bcc875c269bbf2828db3c4e36cabe259f2211ac
collo <netcollo@gmail.com>
2019-06-19 22:55:31 +0800

    add book.py with viewModel

--- app/web/book.py
+++ app/web/book.py
@@ -3,6 +3,7 @@ from app.forms.book import SearchForm
 
 from app.spider.yushu_book import YuShuBook
 from app.libs.helper import is_isbn_or_key
+from view_modells.book import BookViewModel
 from . import web
 
 @web.route('/book/search')
@@ -20,8 +21,10 @@ def search():
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
             result = YuShuBook.search_by_isbn(q)
+            result = BookViewModel.package_single(result, q)
         else:
             result = YuShuBook.search_by_keyword(q, page)
+            result = BookViewModel.package_collection(result, q)
         return jsonify(result)
     else:
         return jsonify(form.errors)
